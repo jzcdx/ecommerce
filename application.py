f6af85918d1bbb5e54f0ae879a5b6509cff063b6
MicroOptimization <jackiezhen538@gmail.com>
2023-03-24 16:06:52 -0400

    outter structure coming together

--- application.py
+++ application.py
@@ -1,1 +1,10 @@
-print("hello world");
+from flask import Flask, render_template
+
+application = Flask(__name__)
+
+@application.route("/")
+def hello_world():
+    return render_template("index.html");
+
+if __name__ == "__main__":
+    application.run(debug=True, use_reloader=True, threaded=True)
