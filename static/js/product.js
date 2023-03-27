1af02296ce8866bf56d3e486b8206e21e70607c7
MicroOptimization <jackiezhen538@gmail.com>
2023-03-27 07:23:51 -0400

    ensured inability to add <= 0 items to cart in product.js, might change that to a serverside check later

--- static/js/product.js
+++ static/js/product.js
@@ -1,20 +1,40 @@
 function addToCart(productId, productName) {
-    console.log("adding " + productId + " to cart " + productName);
+    var productQtty = document.getElementById("qttyInput").value
+    if (productQtty > 0) {
+        console.log("adding " + productId + " to cart " + productName);
+        var newCartItemData = {
+            "product_qtty": productQtty,
+            "product_name": productName, 
+            "product_id": productId
+        };
 
-    var newItemData = {
-        "product_name": productName, 
-        "product_id": productId
-    };
+        $.ajax({
+            type: "POST", //method
+            url: "/addToCart", //this is the flask route
+            data: JSON.stringify(newCartItemData), //I have no idea what this is
+            contentType: "application/json",
+            dataType: 'json', 
+            success: function(result) { //when the response comes back and it's successful, run the code below
+                //This updates the description with the new value from the textarea
+                console.log("request made");
+            } 
+        });
+    } else {
+        console.log("req not sent, prod qtty <= 0");
+    }
+}
 
-    $.ajax({
-        type: "POST", //method
-        url: "/addToCart", //this is the flask route
-        data: JSON.stringify(newItemData), //I have no idea what this is
-        contentType: "application/json",
-        dataType: 'json', 
-        success: function(result) { //when the response comes back and it's successful, run the code below
-            //This updates the description with the new value from the textarea
-            console.log("request made");
-        } 
-    });
+function increaseQtty() {
+    var qttyInput = document.getElementById("qttyInput");
+    var curQtty = parseInt(qttyInput.value);
+
+    qttyInput.value = curQtty + 1;
+}
+
+function decreaseQtty() {
+    var qttyInput = document.getElementById("qttyInput");
+    var curQtty = parseInt(qttyInput.value);
+    if (curQtty > 1) {
+        qttyInput.value = curQtty - 1;
+    }
 }
