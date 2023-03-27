a5a0181bbc9938f47a0672a7505b511e92bdc877
MicroOptimization <jackiezhen538@gmail.com>
2023-03-27 06:56:20 -0400

    ajax request has been made successfully, rest api now receives name and id of product being added to cart

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, jsonify
 from db_utils import DBH
 application = Flask(__name__)
 
@@ -23,9 +23,15 @@ def product_page(product_id):
 def error_page():
     return render_template("error.html");
 
-@application.route("/addToCart/<int:product_id>")
-def add_to_cart(product_id):
-    print("adding " + product_id + " to cart")
+@application.route("/addToCart", methods=["POST"])
+def add_to_cart():
+    if request.method == "POST":
+        desc_data = request.get_json()
+        product_name = desc_data["product_name"]
+        product_id = desc_data["product_id"]
+        print("adding " , product_id , " to cart " , product_name);
+    results = {'updated': 'true'}
+    return jsonify(results);
 
 if __name__ == "__main__":
     application.run(debug=True, use_reloader=True, threaded=True)
--- static/js/product.js
+++ static/js/product.js
@@ -1,3 +1,20 @@
-function addToCart(productId) {
-    console.log("adding " + productId + " to cart")
+function addToCart(productId, productName) {
+    console.log("adding " + productId + " to cart " + productName);
+
+    var newItemData = {
+        "product_name": productName, 
+        "product_id": productId
+    };
+
+    $.ajax({
+        type: "POST", //method
+        url: "/addToCart", //this is the flask route
+        data: JSON.stringify(newItemData), //I have no idea what this is
+        contentType: "application/json",
+        dataType: 'json', 
+        success: function(result) { //when the response comes back and it's successful, run the code below
+            //This updates the description with the new value from the textarea
+            console.log("request made");
+        } 
+    });
 }
